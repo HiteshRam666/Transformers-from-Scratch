f9ff45368f3646e2a6d0467e2d4e9bf0424e6037
Hitesh Ram <116026459+HiteshRam666@users.noreply.github.com>
2025-01-03 20:53:21 +0530

    Add files via upload

---
+++ DecoderBlock.py
@@ -0,0 +1,22 @@
+# Building Decoder Block
+class DecoderBlock(nn.Module):
+  # The DecoderBlock takes in two MultiHeadAttentionBlock. One is self-attention, while the other is cross-attention.
+  # It also takes in the feed-forward block and the dropout rate
+  def __init__(self,  self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+      super().__init__()
+      self.self_attention_block = self_attention_block
+      self.cross_attention_block = cross_attention_block
+      self.feed_forward_block = feed_forward_block
+      self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)]) # List of three Residual Connections with dropout rate
+
+  def forward(self, x, encoder_output, src_mask, tgt_mask):
+
+      # Self-Attention block with query, key, and value plus the target language mask
+      x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask))
+
+      # The Cross-Attention block using two 'encoder_ouput's for key and value plus the source language mask. It also takes in 'x' for Decoder queries
+      x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+
+      # Feed-forward block with residual connections
+      x = self.residual_connections[2](x, self.feed_forward_block)
+      return x
